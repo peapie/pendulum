ad995e9816e08bd2157d201296d06819da52e0ab
Stephanie <stefunny@blueberry.local>
2016-04-29 16:38:16 -0400

    initial commit

---
+++ db/seeds.rb
@@ -0,0 +1,7 @@
+Item.create!(name: "Muladhara",		description: "Life")
+Item.create!(name: "Svadishthana", 	description: "Order")
+Item.create!(name: "Manipura", 		description: "Wisdom")
+Item.create!(name: "Anahata", 		description: "Love")
+Item.create!(name: "Visudda", 		description: "Power")
+Item.create!(name: "Ajna", 		description: "Imagination")
+Item.create!(name: "Sahasrara", 	description: "Understanding & Will")
